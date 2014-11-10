6068adc48e3ffd5a0307bad47cac6e46b8eb37fa
Mitch Campbell <mitchryancampbell@gmail.com>
2014-11-10 13:18:49 +1100

    LESGO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5d890cea7e525d014b25fb77e20dd0ed6413330e5fa1fb70e843d1e8f3de3fa8391aff3ba7a4b04bc3e20fda5985aa9126b50a3d61beb28a4efe9f15a65b1d63'
